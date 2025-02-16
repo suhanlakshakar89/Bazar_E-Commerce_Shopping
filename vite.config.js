6da127bcd5152ce66af33c9001f1504fa2fa6bf3
suhanlakshakar89 <suhanlakshakar@gmail.com>
2025-02-17 01:21:25 +0530

    Bazar

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  Base: "/Bazar_E-Commorce_Shopping/",
+  Base: "/Bazar_Shopping_E-Commerce/",
   plugins: [react()],
 })
